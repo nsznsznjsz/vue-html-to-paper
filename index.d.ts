3013dce3e110e2b97eb68dc7ee87d3025f353726
Celeste <yuan.zx@outlook.com>
2020-02-13 21:57:34 +0800

    types: support local option

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,23 @@
-declare module "vue-html-to-paper" {
-  import { PluginFunction } from "vue";
+import { PluginFunction } from "vue"
 
-  /**
-   * See https://www.w3schools.com/Jsref/met_win_open.asp for more information
-   */
-  interface Options {
-    name?: string;
-    specs?: string[];
-    styles?: string[];
-    replace?: boolean;
-  }
+/**
+ * See https://www.w3schools.com/Jsref/met_win_open.asp for more information
+ */
+interface Options {
+  name?: string
+  specs?: string[]
+  styles?: string[]
+  replace?: boolean
+}
 
-  interface HtmlToPaper {
-    (id: string): void;
-    (id: string, callback?: () => void): void;
-  }
+interface HtmlToPaper {
+  (id: string, localOptions: Options, callback?: () => void): void
+}
 
-  export const VueHtmlToPaper: PluginFunction<Options>;
+export const VueHtmlToPaper: PluginFunction<Options>
 
-  module "vue/types/vue" {
-    interface Vue {
-      $htmlToPaper: HtmlToPaper;
-    }
+module "vue/types/vue" {
+  interface Vue {
+    $htmlToPaper: HtmlToPaper
   }
 }
